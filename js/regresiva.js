65abbed982acc575140189fa4146ce2c2fe53117
Emprende-EnCasa <64992928+Emprende-EnCasa@users.noreply.github.com>
2020-05-13 18:06:58 -0500

    Update regresiva.js

--- js/regresiva.js
+++ js/regresiva.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
             year: 2020, // required
             month: 5, // required
-            day: 12, // required
+            day: 15, // required
             hours: 12, // Default is 0 [0-23] integer
             minutes:30, // Default is 0 [0-59] integer
             seconds: 0, // Default is 0 [0-59] integer
